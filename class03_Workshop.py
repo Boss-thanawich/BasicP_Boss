1717e25a396473dcfd2a2baa9e6c56854191393f
Boss-thanawich <bossmini007@gmail.com>
2025-07-23 13:28:23 +0700

    BasicP_class03

--- class03_Workshop.py
+++ class03_Workshop.py
@@ -13,11 +13,13 @@ while game:
     if (x == 1):
          loop = int(input("จะตีมอนกี่รอบ"))
          for i in range(loop):
+            print("---------------------------------------")
             print("เลือดมอนเตอร์:",mon)
             print("จำนวนครั้งที่เหลือในการตี: ",loop - i)
             print("[1] weapon1 damage =",w1)
             print("[2] weapon2 damage =",w2)
             print("[3] weapon3 damage =",w3)
+            print("---------------------------------------")
             y = int(input("เลือกอาวุธ: "))
         
             if y == 1:
@@ -27,12 +29,27 @@ while game:
                     mon = 20
             elif y == 2:
                 mon-=w2
+                if (mon < 0):
+                    print("ตีแรงเกินไป ฮีลเป็น 20")
+                    mon = 20
+                
             elif y == 3:
                 mon-=w3
+                if (mon < 0):
+                    print("ตีแรงเกินไป ฮีลเป็น 20")
+                    mon = 20
                 
-                if i == 0:
-                    print("เลือดมอนเหลือ:",mon)
-                    game = False
+         if mon != 0 : 
+            print("---------------------------------------")     
+            print("เลือดมอนเหลือ:",mon)
+            print("---------------------------------------")
+            game = False
+         elif mon == 0:
+            print("---------------------------------------")
+            print("เลือดมอนเตอร์เหลือ : 0")
+            print("ชนะแล้วววววว")
+            print("---------------------------------------")
+            game = False
     else :
         print("บายยยย")
         break
